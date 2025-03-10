b438407b78271d8f7203e7e5c4d6934aee8a5bbc
Jan eudald Vaquer <jvaquer@c2r1s3.42prague.com>
2025-03-10 13:12:34 +0100

    fix last name

--- module1/ex03/whatsyourname.py
+++ module1/ex03/whatsyourname.py
@@ -1,7 +1,7 @@
 def main():
 	first_name = input("Hey, what's your first name?: ")
 	last_name = input("And your last name?: ")
-	print(f"Well, pleased to meet you, {first_name.strip()} {last_name}.")
+	print(f"Well, pleased to meet you, {first_name.strip()} {last_name.strip()}.")
 
 if __name__ == '__main__':
 	main()
